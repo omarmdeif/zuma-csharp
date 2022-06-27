f2ef400db736d3b0192b17969f71ee875996018a
omarmdeif <omar.mohamed147@msa.edu.eg>
2022-06-28 00:32:04 +0200

    .

--- ZimZimma/Form1.cs
+++ ZimZimma/Form1.cs
@@ -12,25 +12,58 @@ namespace ZimZimma
 {
     public partial class Form1 : Form
     {
+
+        public Bitmap off;
+        public Bitmap map;
+        public Timer tt = new Timer();
         public Form1()
         {
             this.WindowState = FormWindowState.Maximized;
             this.Load += Form1_Load;
-            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.KeyDown += Form1_KeyDown;
+            tt.Tick += Tt_Tick;
+        }
+
+        private void Tt_Tick(object sender, EventArgs e)
+        {
+            drawd(CreateGraphics());
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            drawd(CreateGraphics());
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawd(CreateGraphics());
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            off = new Bitmap(ClientSize.Width, ClientSize.Height);
+            map = new Bitmap("C:\\Users\\Omar\\source\\repos\\ZimZimma\\ZimZimma\\bin\\bitmapimgs\\Riverbed.bmp");
+            drawd(CreateGraphics());
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        void draw(Graphics g)
         {
-            MessageBox.Show($"{ClientSize.Width}");
+            g.Clear(Color.White);
+            g.DrawImage(map, 0, 0);
+
+
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        void drawd(Graphics g)
         {
-            MessageBox.Show($"{ClientSize.Height}");
+            Graphics g2 = Graphics.FromImage(off);
+            draw(g2);
+
 
         }
     }
